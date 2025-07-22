16ce08cfd668a1f1f66d309326fe29f6c225d33b
avinashvishukinesiis <avinash@kinesiis.com>
2025-07-22 18:19:38 +0530

    new changes

--- Components/HomePage/Section1.tsx
+++ Components/HomePage/Section1.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 const Section1 = () => {
   return (
-    <div className="w-full h-screen bg-[url('/cowImg.png')] bg-cover bg-center bg-no-repeat">
-        kuhkj
-    </div>
+    <>
+      <div className="w-full h-screen bg-[url('/cowImg.png')] bg-cover bg-center bg-no-repeat">
+        <div className='w-full h-max py-2 px-20'>
+            
+        </div>
+      </div>
+    </>
+
   )
 }
 
